cecfeb97881434035b8114414d41c67f6bed244d
mahsumarik <mahsumarik311@gmail.com>
2025-05-24 03:05:15 +0300

    frontend env değişti for deploy

--- gsmFrontend/src/services/api.js
+++ gsmFrontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",   // Spring’in portu‑kökü
+  baseURL: process.env.REACT_APP_API_URL + "/api",
 });
 
-// Her istek öncesi localStorage’daki JWT’yi tak
+// Her istek öncesi localStorage'daki JWT'yi tak
 api.interceptors.request.use((config) => {
-  // Tarayıcıdaki localStorage'dan JWT token’ını alıyoruz. Eğer kullanıcı daha önce giriş yaptıysa ve token’ı sakladıysa, bu token her isteğe eklenir.
+  // Tarayıcıdaki localStorage'dan JWT token'ını alıyoruz. Eğer kullanıcı daha önce giriş yaptıysa ve token'ı sakladıysa, bu token her isteğe eklenir.
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
