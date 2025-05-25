d31d342e0cb2e3742e4bdd49437c95fcf4bf7288
mahsumarik <mahsumarik311@gmail.com>
2025-05-25 16:04:09 +0300

    options izin verildi çözüm.

--- gsmBackend/src/main/java/com/iztech/gsmBackend/config/SecurityConfig.java
+++ gsmBackend/src/main/java/com/iztech/gsmBackend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.iztech.gsmBackend.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,6 +31,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .requestMatchers("/api/auth/**").permitAll()
                     .requestMatchers("/api/advisors/**").permitAll()
                     .requestMatchers("/api/secretary/**").permitAll()
